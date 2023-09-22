7232e225b6fa09c3a2f526dc98af0a50423fb203
Roberto Zambrano <rzajr868@gmail.com>
2023-09-22 17:01:28 +0200

    Hemos definido la interficie en Scene builder

--- Project/src/ControllerDesktop.java
+++ Project/src/ControllerDesktop.java
@@ -1,4 +1,16 @@
-public class ControllerDesktop {
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
+public class ControllerDesktop {
+    @FXML
+    private ChoiceBox<String> choiceBox;
+
+    @FXML
+    private VBox yPane;
+
+    @FXML
+    private AnchorPane info;
 
 }
